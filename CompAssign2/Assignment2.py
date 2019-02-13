89aec2306e8927e17ba010dcede7f67b70505200
fridafemling <jbtmM15dgh>
2019-02-13 13:33:08 +0100

    Testing my card class

--- CompAssign2/Assignment2.py
+++ CompAssign2/Assignment2.py
@@ -1,1 +1,33 @@
-print("Hello just testing intergration through Github")
+
+""" This is an assignment in course Object Oriented Programming in Python - DAT171 """
+__author__ = "Lucas Jutvik & Frida Femling"
+
+
+"""" 
+The task is set up as a top-down design, where the top level functions are written before 
+the core functionality is implemented. Insert placeholders as you go along!
+You are not required to follow the provided task order. On the next page are a 
+specifications of what is required of your library.
+"""
+
+
+# --- Variable declaration ---
+
+
+class Card(object):
+
+    suit_names = ["Clubs", "Hearts", "Diamond","Spades" ]
+    card_number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+
+    def __init__(self, suit_name, card_number):
+        self.self = self
+        self.suit_names = suit_name
+        self.card_number = card_number
+
+    def print_card_number(self):
+        return(self.card_number)
+
+#testing
+my_card = Card("Clubs", "Jack")
+print(my_card.print_card_number())
---
+++ CompAssign2/full_house_straight_flush.py
@@ -0,0 +1,44 @@
+from collections import Counter # Counter is convenient for counting objects (a specialized dictionary)
+
+def check_straight_flush(cards):
+    """
+    Checks for the best straight flush in a list of cards (may be more than just 5)
+
+    :param cards: A list of playing cards.
+    :return: None if no straight flush is found, else the value of the top card.
+    """
+    vals = [(c.give_value(), c.suit) for c in cards] \
+        + [(1, c.suit) for c in cards if c.give_value() == 14]  # Add the aces!
+    for c in reversed(cards): # Starting point (high card)
+        # Check if we have the value - k in the set of cards:
+        found_straight = True
+        for k in range(1, 5):
+            if (c.give_value() - k, c.suit) not in vals:
+                found_straight = False
+                break
+        if found_straight:
+            return c.give_value()
+
+
+def check_full_house(cards):
+    """
+    Checks for the best full house in a list of cards (may be more than just 5)
+
+    :param cards: A list of playing cards
+    :return: None if no full house is found, else a tuple of the values of the triple and pair.
+    """
+    value_count = Counter()
+    for c in cards:
+        value_count[c.give_value()] += 1
+    # Find the card ranks that have at least three of a kind
+    threes = [v[0] for v in value_count.items() if v[1] >= 3]
+    threes.sort()
+    # Find the card ranks that have at least a pair
+    twos = [v[0] for v in value_count.items() if v[1] >= 2]
+    twos.sort()
+
+    # Threes are dominant in full house, lets check that value first:
+    for three in reversed(threes):
+        for two in reversed(twos):
+            if two != three:
+                return three, two
